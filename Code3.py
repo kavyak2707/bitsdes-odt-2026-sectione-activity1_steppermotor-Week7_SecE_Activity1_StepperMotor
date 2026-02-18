ee6a3a77b6f4034b7cb2c33fe87fea34f3bf5c6c
kavyak2707 <kavyak2707@gmail.com>
2026-02-18 14:55:36 +0530

    Update Code3.py

--- Code3.py
+++ Code3.py
@@ -1,1 +1,29 @@
+from machine import Pin
+import time
 
+stepm1 = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
+in1=Pin(14, Pin.OUT)
+in2=Pin(25, Pin.OUT)
+in3=Pin(26, Pin.OUT)
+in4=Pin(27, Pin.OUT)
+my_t=0.005
+
+while True:
+    for i in range(0,501):
+        for i in stepm1:
+            in1.value(i[0])
+            in2.value(i[1])
+            in3.value(i[2])
+            in4.value(i[3])
+                
+            time.sleep(my_t)
+            
+    for i in range(500,1001):
+        for i in reversed (stepm1):
+            in1.value(i[0])
+            in2.value(i[1])
+            in3.value(i[2])
+            in4.value(i[3])
+                
+            time.sleep(my_t)
