06061df7d82625b16e5559e0d12e6034bf32995c
kavyak2707 <kavyak2707@gmail.com>
2026-02-18 14:42:55 +0530

    Update Code2.py

--- Code2.py
+++ Code2.py
@@ -1,1 +1,21 @@
+from machine import Pin
+import time
+
+stepm1 = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
+in1=Pin(14, Pin.OUT)
+in2=Pin(25, Pin.OUT)
+in3=Pin(26, Pin.OUT)
+in4=Pin(27, Pin.OUT)
+my_t=0.005
+ 
+
+for i in range(0,1000):
+    for i in stepm1:
+        in1.value(i[0])
+        in2.value(i[1])
+        in3.value(i[2])
+        in4.value(i[3])
+            
+        time.sleep(my_t)
 
